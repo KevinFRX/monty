e5dfb5418dc8d7d4ea19c7067db305c3e7bb4937
KevinFRX <xerokf13@gmail.com>
2022-05-12 10:18:40 -0700

    the last one I swear

--- pstr.c
+++ pstr.c
@@ -12,10 +12,9 @@ void _pstr(stack_t **stack, unsigned int line)
 	stack_t *cnode = *stack;
 	(void)line;
 
-	while (cnode != NULL && cnode->n != 0)
+	while (cnode != NULL && cnode->n > 31 && cnode->n < 128)
 	{
-		if (cnode->n > 31 && cnode->n < 128)
-			printf("%c", cnode->n);
+		printf("%c", cnode->n);
 		cnode = cnode->next;
 	}
 	printf("\n");
