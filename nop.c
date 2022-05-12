ec7245b90f787757e2c904c734cbd7ac9844858a
KevinFRX <xerokf13@gmail.com>
2022-05-11 19:34:38 -0700

    dop opcode

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * stack_nop - doesn't do anything
+ * @stack: the stack
+ * @line: the line where the opcode is
+ */
+void _nop(stack_t **stack, unsigned int line)
+{
+	(void)stack;
+	(void)line;
+}
